b4ddff3be9d151d54c16543c0437287fee3c68f0
Dor Refaeli <dorefaeli@gmail.com>
2020-06-23 14:53:46 +0300

    Controls now works, just doesn't send data to server

--- app/src/main/java/com/example/flightmobileapp/SimulatorActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/SimulatorActivity.kt
@@ -5,14 +5,20 @@ import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
 import com.google.gson.GsonBuilder
+import io.github.controlwear.virtual.joystick.android.JoystickView
+import kotlinx.android.synthetic.main.activity_simulator.*
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import kotlin.math.cos
+import kotlin.math.sin
 
 class SimulatorActivity : AppCompatActivity() {
     private var client = ClientConnect()
@@ -28,6 +34,66 @@ class SimulatorActivity : AppCompatActivity() {
             image = findViewById(R.id.screen_shoot)
             startShowScreenShoots(url)
         }
+
+        setSeekBars()
+        setJoystick()
+
+    }
+
+    private fun setSeekBars() {
+        var rudderSeekBar = findViewById<SeekBar>(R.id.seekBarRudder)
+        rudderSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                //There is no need to check if the margin is more then 1%
+                val realProgress = (progress - 50) / 50.0
+                //TODO send realProgress value to server
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+
+        var throttleSeekBar = findViewById<SeekBar>(R.id.seekBarThrottle)
+        throttleSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                //There is no need to check if the margin is more then 1%
+                val realProgress = progress / 100.0
+                //TODO send realProgress value to server
+            }
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
+    }
+
+    private fun setJoystick() {
+        var joystickView = findViewById<JoystickView>(R.id.joystickView)
+        joystickView.setOnMoveListener(object : JoystickView.OnMoveListener{
+            var currAileron = 0.0
+            var currElevator = 0.0
+            override fun onMove(angle: Int, strength: Int) {
+                if (angle == 0 && strength == 0) {
+                    this.currAileron = 0.0
+                    this.currElevator = 0.0
+                    //TODO send currAileron to server
+                    //TODO send currElevator to server
+                } else {
+                    val realStrength = strength / 100.0
+                    val angleInRadians = angle * Math.PI / 180.0
+                    val aileron = realStrength * cos(angleInRadians)
+                    val elevator = realStrength * sin(angleInRadians)
+                    //Check if margin is more then 1%
+                    val aileronDelta = currAileron - aileron
+                    val elevatorDelta = currElevator - elevator
+                    if (aileronDelta > 0.01 || aileronDelta < -0.01) {
+                        currAileron = aileron
+                        //TODO send currAileron to server
+                    }
+                    if (elevatorDelta > 0.01 || elevatorDelta < -0.01) {
+                        currElevator = elevator
+                        //TODO send currElevator to server
+                    }
+                }
+            }
+        })
     }
 
     private fun startShowScreenShoots(url : String) {
