4691a6ec7a5d1efaea3863e97b8a2804a26944e8
Dor Refaeli <dorefaeli@gmail.com>
2020-06-25 09:48:38 +0300

    Code clean up before submitting

--- app/src/main/java/com/example/flightmobileapp/SimulatorActivity.kt
+++ app/src/main/java/com/example/flightmobileapp/SimulatorActivity.kt
@@ -2,27 +2,15 @@ package com.example.flightmobileapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.SeekBar
-import android.widget.TextView
-import androidx.activity.OnBackPressedCallback
-import com.google.gson.GsonBuilder
 import io.github.controlwear.virtual.joystick.android.JoystickView
-import kotlinx.android.synthetic.main.activity_simulator.*
-import okhttp3.ResponseBody
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 import kotlin.math.cos
 import kotlin.math.sin
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import java.util.concurrent.ThreadLocalRandom
 
 class SimulatorActivity : AppCompatActivity() {
     private var loopGetImage = false
@@ -50,7 +38,7 @@ class SimulatorActivity : AppCompatActivity() {
     }
 
     private fun setSeekBars() {
-        var rudderSeekBar = findViewById<SeekBar>(R.id.seekBarRudder)
+        val rudderSeekBar = findViewById<SeekBar>(R.id.seekBarRudder)
         rudderSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 //There is no need to check if the margin is more then 1%
@@ -62,7 +50,7 @@ class SimulatorActivity : AppCompatActivity() {
             override fun onStopTrackingTouch(seekBar: SeekBar?) {}
         })
 
-        var throttleSeekBar = findViewById<SeekBar>(R.id.seekBarThrottle)
+        val throttleSeekBar = findViewById<SeekBar>(R.id.seekBarThrottle)
         throttleSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 //There is no need to check if the margin is more then 1%
@@ -76,36 +64,28 @@ class SimulatorActivity : AppCompatActivity() {
     }
 
     private fun setJoystick() {
-        var joystickView = findViewById<JoystickView>(R.id.joystickView)
+        val joystickView = findViewById<JoystickView>(R.id.joystickView)
         joystickView.setOnMoveListener(object : JoystickView.OnMoveListener{
             var currAileron = 0.0
             var currElevator = 0.0
             override fun onMove(angle: Int, strength: Int) {
                 var changed = false
-                if (angle == 0 && strength == 0) {
-                    this.currAileron = 0.0
-                    this.currElevator = 0.0
-                    client.setAileron(0.0)
-                    client.setAileron(0.0)
+                val realStrength = strength / 100.0
+                val angleInRadians = angle * Math.PI / 180.0
+                val aileron = realStrength * cos(angleInRadians)
+                val elevator = realStrength * sin(angleInRadians)
+                //Check if margin is more then 1%
+                val aileronDelta = currAileron - aileron
+                val elevatorDelta = currElevator - elevator
+                if (aileronDelta > 0.01 || aileronDelta < -0.01 || aileron == 0.0) {
+                    currAileron = aileron
+                    client.setAileron(currAileron)
                     changed = true
-                } else {
-                    val realStrength = strength / 100.0
-                    val angleInRadians = angle * Math.PI / 180.0
-                    val aileron = realStrength * cos(angleInRadians)
-                    val elevator = realStrength * sin(angleInRadians)
-                    //Check if margin is more then 1%
-                    val aileronDelta = currAileron - aileron
-                    val elevatorDelta = currElevator - elevator
-                    if (aileronDelta > 0.01 || aileronDelta < -0.01) {
-                        currAileron = aileron
-                        client.setAileron(currAileron)
-                        changed = true
-                    }
-                    if (elevatorDelta > 0.01 || elevatorDelta < -0.01) {
-                        currElevator = elevator
-                        client.setElevator(currElevator)
-                        changed = true
-                    }
+                }
+                if (elevatorDelta > 0.01 || elevatorDelta < -0.01 || elevator == 0.0) {
+                    currElevator = elevator
+                    client.setElevator(currElevator)
+                    changed = true
                 }
                 if (changed) {
                     client.sendCommand()
@@ -138,7 +118,7 @@ class SimulatorActivity : AppCompatActivity() {
     override fun onResume(){
         super.onResume()
         this.client.flagSimulatorActivity = true
-        this.loopGetImage=true;
+        this.loopGetImage=true
         startShowScreenShoots()
     }
 
